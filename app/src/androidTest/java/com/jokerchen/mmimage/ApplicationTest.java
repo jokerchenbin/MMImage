0a96cb62fcfff9a1fb3b5a79c54891c899d43cfb
bin_chen <bin_chen@loongjoy.com>
2016-07-04 11:37:48 +0800

    Initial commit

---
+++ app/src/androidTest/java/com/jokerchen/mmimage/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.jokerchen.mmimage;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
